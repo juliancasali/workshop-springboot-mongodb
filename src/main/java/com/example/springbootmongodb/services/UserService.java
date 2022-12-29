5f1399bf40f869d5596b7ce87c6c394ac842e91c
juliancasali <juliancasalii@gmail.com>
2022-12-28 22:08:12 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/example/springbootmongodb/services/UserService.java
@@ -0,0 +1,20 @@
+package com.example.springbootmongodb.services;
+
+import com.example.springbootmongodb.domain.User;
+import com.example.springbootmongodb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+}
