b93daae0ff47cc5fbcb73e31133aca9f30a893cd
juliancasali <juliancasalii@gmail.com>
2023-01-01 13:53:29 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/example/springbootmongodb/domain/Post.java
+++ src/main/java/com/example/springbootmongodb/domain/Post.java
@@ -1,12 +1,15 @@
 package com.example.springbootmongodb.domain;
 
 import com.example.springbootmongodb.dto.AuthorDTO;
+import com.example.springbootmongodb.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -20,6 +23,8 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private final List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
     }
@@ -70,6 +75,10 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
     }
 
     @Override
